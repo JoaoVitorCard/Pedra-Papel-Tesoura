a12b4b55b99be53e6a29956e2b93d92b8a89f827
João Vitor <joaovitonrnascim@gmail.com>
2023-06-26 18:29:06 -0300

    Modified the result of game

--- script.js
+++ script.js
@@ -14,15 +14,26 @@ const rockChoice = () => {
   const machineChoice = Math.floor(Math.random() * 3) + 1
 
   if (choiceNumber === machineChoice) {
-    alert("Empate!")
+    alert(
+      ` Empate!
+        Sua escolha: Pedra
+        Escolha da máquina: Pedra`
+    )
     drawScore++
     draw.innerHTML = drawScore
   } else if (choiceNumber != machineChoice && machineChoice === 2) {
-    alert("Você Perdeu!")
+    alert(
+      ` Você perdeu!
+        Sua escolha: Pedra
+        Escolha da máquina: Papel`)
     machineScore++
     machine.innerHTML = machineScore
   } else if (choiceNumber != machineChoice && machineChoice === 3) {
-    alert("Você ganhou!")
+    alert(
+      ` Você ganhou! 
+        Sua escolha: Pedra
+        Escolha da máquina: Tesoura`
+    )
     personalScore++
     myScore.innerHTML = personalScore
   }
@@ -33,15 +44,27 @@ const paperChoice = () => {
   const machineChoice = Math.floor(Math.random() * 3) + 1
 
   if (choiceNumber === machineChoice) {
-    alert("Empate!")
+    alert(
+      ` Empate 
+        Sua escolha: Papel
+        Escolha da máquina: Papel`
+    )
     drawScore++
     draw.innerHTML = drawScore
   } else if (choiceNumber != machineChoice && machineChoice === 1) {
-    alert("Você ganhou!")
+    alert(
+      ` Você ganhou!
+        Sua escolha: Papel
+        Escolha da máquina: Pedra`
+    )
     personalScore++
     myScore.innerHTML = personalScore
   } else if (choiceNumber != machineChoice && machineChoice === 3) {
-    alert("Você perdeu!")
+    alert(
+      ` Você perdeu! 
+        Sua escolha: Papel
+        Escolha da máquina: Tesoura`
+    )
     machineScore++
     machine.innerHTML = machineScore
   }
@@ -52,15 +75,27 @@ const scissorsChoice = () => {
   const machineChoice = Math.floor(Math.random() * 3) + 1
 
   if (choiceNumber === machineChoice) {
-    alert("Empate")
+    alert(
+      ` Empate 
+        Sua escolha: Tesoura
+        Escolha da máquina: Tesoura`
+    )
     drawScore++
     draw.innerHTML = drawScore
   } else if (choiceNumber != machineChoice && machineChoice === 1) {
-    alert("Você perdeu!")
+    alert(
+      ` Você perdeu! 
+        Sua escolha: Tesoura
+        Escolha da máquina: Pedra`
+    )
     machineScore++
     machine.innerHTML = machineScore
   } else if (choiceNumber != machineChoice && machineChoice === 2) {
-    alert("Você venceu!")
+    alert(
+      ` Você ganhou! 
+        Sua escolha: Tesoura
+        Escolha da máquina: Papel`
+    )
     personalScore++
     myScore.innerHTML = personalScore
   }
